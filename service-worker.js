ed1fd7e73e5178ace78daeabd2825313aea85521
AndrewProkopenko <i.andrewprokopenko@gmail.com>
2020-09-22 14:06:03 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio2/precache-manifest.701250ea515654b64379c713dc980814.js"
+  "/portfolio2/precache-manifest.307e6aab257463b5ccfc9e5ad0d4c97c.js"
 );
 
 self.addEventListener('message', (event) => {
